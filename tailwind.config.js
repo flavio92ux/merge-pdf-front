d4ed79ef6924d751319eba03a5e9650ff90f6466
Flavio F <flavio.filho@sada.com.br>
2024-11-21 08:35:26 -0300

    Define initial designsystem

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,34 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      ...theme,
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
+      colors: {
+        ...theme.colors,
+        white: '#ffffff',
+        blue: {
+          500: '#177EF3',
+          700: '#0259BB'
+        },
+        blueGray: {
+          500: '#435265'
+        },
+        gray: {
+          100: '#E2ECF2',
+          200: '#DEE5E9',
+          300: '#D6D4D4'
+        },
+      },
+      fontSize: {
+        'small': '12px',
+        'medium': '14px',
+        'large': '16px'
+      },
+    },
   },
   plugins: [],
 }
